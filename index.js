ecebba2789167adaee69b8a3e8e0a5dd41893fc1
Veronika Luthar <17022@bayfield-high.school.nz>
2020-06-22 17:12:44 +1200

    Commented Javascript

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // JavaScript source code
 
-
-
+// displaying the menu items
 let newDiv = [];
 let newButton = [];
 let info = [];
 let itemNumber = [];
 
+
+// morning tea menu
 let morningTea = [];
 
 morningTea.push(new menuItems("Cheese Roll", 4, "Morning Tea"));
@@ -18,6 +19,7 @@ morningTea.push(new menuItems("Cheese Pulls", 3, "Morning Tea"));
 morningTea.push(new menuItems("Sandwiches", 4, "Morning Tea"));
 morningTea.push(new menuItems("Bagels", 4, "Morning Tea"));
 
+// lunch menu for week one
 let weekOne = [];
 
 weekOne.push(new menuItems("Chicken nuggets with baked chips", 5, "Lunch"));
@@ -26,9 +28,8 @@ weekOne.push(new menuItems("Macaroni and cheese", 5, "Lunch"));
 weekOne.push(new menuItems("Butter chicken on rice", 5, "Lunch"));
 weekOne.push(new menuItems("Fried rice", 5, "Lunch"));
 
-
+// lunch menu for week two
 let weekTwo = [];
-
 
 weekTwo.push(new menuItems("Chicken nuggets with baked chips", 5, "Lunch"));
 weekTwo.push(new menuItems("Bacon, egg, potato and spinach pie", 5, "Lunch"));
@@ -36,27 +37,30 @@ weekTwo.push(new menuItems("Lasagne", 5, "Lunch"));
 weekTwo.push(new menuItems("Meatballs on tomato sauce with rice", 5, "Lunch"));
 weekTwo.push(new menuItems("Chicken or beef burgers with tomato relish and cheese", 5, "Lunch"));
 
-displayMenu("bob");
+// displays the morning tea menu as default
+displayMenu("morning tea");
 
+// function to create menu items and put them in an array
 function menuItems(name, price, type) {
     this.name = name;
     this.price = price;
     this.type = type;
 }
 
+// on button click changes the buttons' styling and menu
 function menuButtonClick(dir) {
     if (dir === 1) {
         document.getElementById("lunch").classList = "button-current";
         document.getElementById("morning-tea").classList = "button-default";
-        displayMenu("notbob");
+        displayMenu("lunch");
     }
     else {
         document.getElementById("morning-tea").classList = "button-current";
         document.getElementById("lunch").classList = "button-default";
-        displayMenu("bob");
+        displayMenu("morning tea");
     }
 }
-
+// creating an element for the menu display
 function createElement(type, obj, name, text) {
     array = document.createElement(type);
     array.classList = name;
@@ -64,11 +68,13 @@ function createElement(type, obj, name, text) {
     return obj.appendChild(array);
 }
 
+// function to display the menu depending on button click
 function displayMenu(menu) {
 
+    // clears "container" contents
     document.getElementById("container").innerHTML = "";
 
-    if (menu === "bob") {
+    if (menu === "morning tea") {
         for (let i = 0; i < morningTea.length; i++) {
             newDiv[i] = new createElement("div", document.getElementById("container"), "item", "");
             newButton[i] = new createElement("button", newDiv[i], "buttons", "ADD");
@@ -87,11 +93,3 @@ function displayMenu(menu) {
         }
     }
 }
-
-
-//document.createElement("d")
-
-
-//for (let i = 0; i < weekOne.length; i++) {
- //   console.log(weekOne[i].name)
-//}
